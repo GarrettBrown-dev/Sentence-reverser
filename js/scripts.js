a18effbd385e1da694df1a9483f2952bd9d70ca6
donnymays <dtmays2@gmail.com>
2020-08-10 15:42:21 -0700

    work on js files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+
+function first(sentence) {
+  return (sentence.charAt(0));
+}
+function last(sentence) {
+  return (sentence.id.substr.charAt(sentence.length-1));
+}
+const sentence = prompt("Write a sentence.");
+const result1 = first(sentence);
+const result2 = last(sentence);
+alert(result1 + result2);
